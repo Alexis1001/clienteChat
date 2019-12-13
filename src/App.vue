8087830b623ec0b3b67ff31fdc86c193ce13bae6
PC <alexisperezgomez1@gmial.com>
2019-12-13 14:16:56 -0600

    chat in vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+   <div >
+     <div >
+       <router-link to="/Login">cerrar secion</router-link>|
+       <!--<button v-on:click="login" >ir </button>-->
+      </div>
+      <router-view/>
+    </div>
 </template>
 
-<script lang="ts">
+<script >
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import register from '@/views/Register.vue';
 
-@Component({
-  components: {
-    HelloWorld,
+export default {
+  components:{
+    register,
   },
-})
-export default class App extends Vue {}
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    login:function(){
+
+      this.$router.push('/Register');
+      
+    }
+  }
+
+
+}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
